0aff392b53739f3b289050b444d2148fdaf5e831
Kevin Goos <kevineke@gmail.com>
2014-07-12 20:39:57 +0200

    - First commit of forge

---
+++ src/main/java/be/ghost606/trademc/TradeMC.java
@@ -0,0 +1,32 @@
+package be.ghost606.trademc;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Kevin on 12/07/2014.
+ */
+
+@Mod(modid = "TradeMC", name = "TradeMC", version = "1.7.2-1.0")
+public class TradeMC {
+
+    @Mod.Instance("TradeMC")
+    public static TradeMC instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
