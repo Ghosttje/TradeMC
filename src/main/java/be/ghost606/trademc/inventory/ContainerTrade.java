81d57df781f60b91cae74f760c762b989aabd651
Kevin Goos <kevineke@gmail.com>
2014-07-17 00:39:34 +0200

    - Working on inventory in GUI

--- src/main/java/be/ghost606/trademc/inventory/ContainerTrade.java
+++ src/main/java/be/ghost606/trademc/inventory/ContainerTrade.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 /**
  * Created by Kevin on 16/07/2014.
@@ -31,7 +32,7 @@ public class ContainerTrade extends Container {
     }
 
     @Override
-    public boolean canInteractWith(EntityPlayer p_75145_1_) {
-        return false;
+    public boolean canInteractWith(EntityPlayer entityPlayer) {
+        return true;
     }
 }
